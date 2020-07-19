4889d923807449351511854c0e858670b83261e0
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-07-19 10:36:33 -0400

    Working on file searching functionality

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,24 @@ struct Search {
     count: u8
 }
 
-fn search_file(word: Search, file: String) -> () {
+impl Search {
+    pub fn add_one(&mut self) -> () {
+        self.count += 1
+    }
+}
+
+fn search_file(search: &mut Search, file: String) -> () {
     let mut file = File::open(file).expect("The File could not be opened");
     let mut file_contents = String::new();
-
     file.read_to_string(&mut file_contents).expect("The file could not be read to a string");
+
+    let words_vector: Vec<&str> = file_contents.trim().split(' ').collect();
+
+    for word in words_vector {
+        if word == search.word {
+            search.add_one()
+        }
+    }
 
     return println!("{}", file_contents);
 }
@@ -30,7 +43,7 @@ fn main() {
         count: 0
     };
 
-    println!("\nWhat file do you want to read?");
+    println!("What file do you want to read?");
     match io::stdin().read_line(&mut file_name) {
         Ok(_) => println!("You've entered {}", file_name),
         Err(_) => println!("There was an error reading your input")
@@ -44,4 +57,6 @@ fn main() {
 
     println!("{}", file_name);
     println!("{}", word_search.word);
+
+    search_file(&word_search, file_name);
 }
